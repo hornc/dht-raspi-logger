0b0cb42d492280632a5d8847a31615d1880fa440
hornc <charles.horn@gmail.com>
2013-10-06 20:32:34 +1300

    add PATH to cron wlan monitor

--- cron/wlan-monitor.sh
+++ cron/wlan-monitor.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # to run this every 15mins add the following line to root user's crontab:
 # */15 * * * * /home/pi/DHT-RaspPi-logger/cron/wlan-monitor.sh
+
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
 if ifconfig wlan0 | grep -q "inet addr:" ; then
   echo "WiFi connected, everything is OK!" 
 else
